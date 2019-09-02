de092d88ba912c884164d22d13c7dde7ddad5006
Anton Cherkasov <antoncherkasov007@gmail.com>
2019-09-03 01:52:23 +0500

    Initial Commit

---
+++ TODOtxt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TODOtxt
+//
+//  Created by subzero on 03/09/2019.
+//  Copyright © 2019 antoncherkasov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
